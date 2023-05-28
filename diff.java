770e0ca7e336f77d2e842d8a4660941fc5dcdb29
Sumedh M Anvekar <sumedhm8888@gmail.com>
2023-05-28 17:54:48 +0530

    changed vale of variable a

--- diff.java
+++ diff.java
@@ -2,7 +2,7 @@ public class diff
 {
     public static void main(String[] args)
     {
-        int a=8,b=7,s;
+        int a=88,b=7,s;
         s=a-b;
         System.out.println("diff is",s);
     }
